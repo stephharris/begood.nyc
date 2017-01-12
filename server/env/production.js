48ce31cfe6efc5a11395eee9c9584f73bc0b418d
Stephanie Harris <310srh@gmail.com>
2017-01-12 15:50:25 -0500

    micro change

---
+++ server/env/production.js
@@ -0,0 +1,3 @@
+module.exports = {
+  DATABASE_URI: 'postgres://aljroytgjuivcy:a50874ed744d315064bf93e674c6042a89d6bb9d5ec5b700c31840efe7d9af81@ec2-54-225-104-61.compute-1.amazonaws.com:5432/d1q6d7tnfa1lv4'
+};
