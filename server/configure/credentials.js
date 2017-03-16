fa568ffd62119f404aeee43db2eaf4deeaa59ca2
Stephanie Harris <310srh@gmail.com>
2017-03-16 12:17:19 -0400

    cchanges

---
+++ server/configure/credentials.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Credentials = {
+  username: 'steph',
+  password: 'steph',
+  jwtSecret: 'somesecretkeyforjsonwebtoken'
+}
+
+module.exports = {
+  Credentials: Credentials
+}
