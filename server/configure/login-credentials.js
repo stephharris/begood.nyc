d51cb95d17c08ce6292076f531f816c917bb6fac
Stephanie Harris <310srh@gmail.com>
2017-03-16 11:38:57 -0400

    gitignore working

---
+++ server/configure/login-credentials.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Credentials = {
+  username: 'Steph',
+  password: 'Steph',
+  jwtSecret: 'somesecretkeyforjsonwebtoken'
+}
+
+module.exports = {
+  Credentials: Credentials
+}
