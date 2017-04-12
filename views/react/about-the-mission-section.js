fd35687da8652c9938c8718a2e9e3536497b6679
Stephanie Harris <310srh@gmail.com>
2017-04-12 16:29:01 -0700

    copy fixes & about/contact layout changes

--- views/react/about-the-mission-section.js
+++ views/react/about-the-mission-section.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-export default class AboutSection extends React.Component {
+export default class AboutTheMissionSection extends React.Component {
 
   constructor(props){
     super(props);
@@ -16,7 +16,7 @@ export default class AboutSection extends React.Component {
 
   onShow(){
     if(this.state.maxHeight === 0){
-      this.setState({ maxHeight: 100, h1Class: 'section-expanded', rotate: 0, opacity: 0, display: 'block'})
+      this.setState({ maxHeight: 350, h1Class: 'section-expanded', rotate: 0, opacity: 0, display: 'block'})
     }else{
       this.setState({ maxHeight: 0, h1Class: 'section-default', rotate: 90, opacity: 1, display: 'none' })
     }
@@ -27,7 +27,7 @@ export default class AboutSection extends React.Component {
       <div onClick={this.onShow.bind(this)} id="about-container">
 
         <div>
-         <h1 className={this.state.h1Class}>{ this.props.content.h1 }</h1>
+         <h1 className={this.state.h1Class}>Our Mission</h1>
           <div className="svgContainer">
             <svg style={{ transform: 'rotate(' + this.state.rotate + 'deg)', opacity: this.state.opacity }} viewBox="0 0 560.3 560.3">
             <polygon points="344.4,215.8 344.4,0 215.8,0 215.8,215.8 0,215.8 0,344.4 215.8,344.4 215.8,560.3 344.4,560.3 344.4,344.4 560.3,344.4 560.3,215.8 "/>
@@ -40,7 +40,11 @@ export default class AboutSection extends React.Component {
         </div>
 
          <div className="aboutSection" style={{ maxHeight: this.state.maxHeight }}>
-           <h3>{ this.props.content.body }</h3>
+           <h3>
+            This platform is based on the deceptively simple idea that finding opportunities to do good and ‘BeGood’ (especially in our current political climate) shouldn’t prove such a challenge. <br/><br/>
+
+            We hold progressive values, believe strongly in the power of local community & diversity, strive to empower individuals to transform their frustrations into action.
+           </h3>
          </div>
 
       </div>
